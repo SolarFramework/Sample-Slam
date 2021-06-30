2af54c770126ceb469b83ae65e2cfab2aeb571b3
BCOM\nduong <nam-duong.duong@b-com.com>
2021-06-30 09:19:46 +0200

    fix sample mono and add options to use popsift and wallis filter

--- SolARSample_SLAM_Mono/main.cpp
+++ SolARSample_SLAM_Mono/main.cpp
@@ -47,9 +47,12 @@
 #include "api/slam/IBootstrapper.h"
 #include "api/slam/ITracking.h"
 #include "api/slam/IMapping.h"
+#include "api/image/IImageConvertor.h"
 
 #define NB_NEWKEYFRAMES_LOOP 10
 #define NB_LOCALKEYFRAMES 10
+#define USING_FILTER 0
+#define USING_POPSIFT 0
 
 using namespace SolAR;
 using namespace SolAR::datastructure;
@@ -127,6 +130,8 @@ int main(int argc, char **argv) {
 		auto tracking = xpcfComponentManager->resolve<slam::ITracking>();
 		LOG_INFO("Resolving mapping");
 		auto mapping = xpcfComponentManager->resolve<slam::IMapping>();
+		LOG_INFO("Resolving image convertor");
+		auto imageConvertor = xpcfComponentManager->resolve<image::IImageConvertor>();
 		LOG_INFO("Loaded all components");
 
 		// initialize pose estimation with the camera intrinsic parameters (please refer to the use of intrinsic parameters file)
@@ -144,7 +149,9 @@ int main(int argc, char **argv) {
 		// get properties
 		float minWeightNeighbor = mapping->bindTo<xpcf::IConfigurable>()->getProperty("minWeightNeighbor")->getFloatingValue();
 		float reprojErrorThreshold = mapManager->bindTo<xpcf::IConfigurable>()->getProperty("reprojErrorThreshold")->getFloatingValue();
+		int hasPose = bootstrapper->bindTo<xpcf::IConfigurable>()->getProperty("hasPose")->getIntegerValue();
 
+		// start camera
 		if (camera->start() != FrameworkReturnCode::_SUCCESS)
 		{
 			LOG_ERROR("Camera cannot start");
@@ -152,62 +159,36 @@ int main(int argc, char **argv) {
 		}
 
         // Load map from file
+		bool bootstrapOk = false;
+		SRef<Keyframe> keyframe2;
+		std::vector<Transform3Df> framePoses;
         if (mapManager->loadFromFile() == FrameworkReturnCode::_SUCCESS)
         {
             LOG_INFO("Load map done!");
+			if (keyframesManager->getKeyframe(0, keyframe2) != FrameworkReturnCode::_SUCCESS)
+				return -1;
+			// Prepare for tracking
+			tracking->updateReferenceKeyframe(keyframe2);
+			framePoses.push_back(keyframe2->getPose());
+			bootstrapOk = true;
         }
         else
         {
             LOG_WARNING("Failed to load map from file");
-        }
-
-        SRef<Keyframe>	keyframe2;
-        if (pointCloudManager->getNbPoints() > 0)
-        {
-            // Map loaded from file and not empty
-            if (keyframesManager->getKeyframe(0, keyframe2) != FrameworkReturnCode::_SUCCESS)
-            {
-                return -1; //FrameworkReturnCode::_ERROR_;
-            }
-        }
-        else
-        {
-            // Either no or empty map files
-            LOG_INFO("Initialization from scratch");
-            SRef<Trackable> trackable;
-            if (trackableLoader->loadTrackable(trackable) != FrameworkReturnCode::_SUCCESS)
-            {
-                LOG_ERROR("cannot load fiducial marker");
-                return -1;
-            }
-            else
-            {
-                if (fiducialMarkerPoseEstimator->setTrackable(trackable)!= FrameworkReturnCode::_SUCCESS)
-                {
-                    LOG_ERROR("cannot set fiducial marker as a trackable ofr fiducial marker pose estimator");
-                    return -1;
-                }
-            }
-            bool bootstrapOk = false;
-            while (!bootstrapOk) {
-                SRef<Image> image, view;
-                camera->getNextImage(image);
-                Transform3Df pose = Transform3Df::Identity();
-                fiducialMarkerPoseEstimator->estimate(image, pose);
-                if (bootstrapper->process(image, view, pose) == FrameworkReturnCode::_SUCCESS) {
-                    double bundleReprojError = bundler->bundleAdjustment(calibration, distortion);
-                    bootstrapOk = true;
-                }
-                if (!pose.isApprox(Transform3Df::Identity()))
-                    overlay3D->draw(pose, view);
-                if (imageViewer->display(view) == SolAR::FrameworkReturnCode::_STOP)
-                    return 1;
-            }
-            keyframesManager->getKeyframe(1, keyframe2);
-        }
-
-		LOG_INFO("Number of initial point cloud: {}", pointCloudManager->getNbPoints());
-		LOG_INFO("Number of initial keyframes: {}", keyframesManager->getNbKeyframes());
+			LOG_INFO("Initialization from scratch");
+			if (hasPose) {
+				SRef<Trackable> trackable;
+				if (trackableLoader->loadTrackable(trackable) != FrameworkReturnCode::_SUCCESS){
+					LOG_ERROR("cannot load fiducial marker");
+					return -1;
+				}
+				if (fiducialMarkerPoseEstimator->setTrackable(trackable) != FrameworkReturnCode::_SUCCESS)
+				{
+					LOG_ERROR("cannot set fiducial marker as a trackable ofr fiducial marker pose estimator");
+					return -1;
+				}
+			}
+        }          	
 
 		// display point cloud function
 		auto fnDisplay = [&keyframesManager, &pointCloudManager, &viewer3DPoints](const std::vector<Transform3Df>& framePoses) {
@@ -227,111 +208,123 @@ int main(int argc, char **argv) {
 				return true;
 		};
 
-		// Prepare for tracking
-		tracking->updateReferenceKeyframe(keyframe2);
-
-		// init display point cloud
-		fnDisplay({ keyframe2->getPose() });
-
 		// Start tracking
 		clock_t start, end;
 		int count = 0;
-		int countNewKeyframes = 0;
-		std::vector<Transform3Df>   framePoses{ keyframe2->getPose() };
+		int countNewKeyframes = 0;		
 		start = clock();
 		while (true)
 		{
-            SRef<Image>											view, filteredView, displayImage;
+            SRef<Image>											view, displayImage;
 			std::vector<Keypoint>								keypoints, undistortedKeypoints;
 			SRef<DescriptorBuffer>                              descriptors;
 			SRef<Frame>                                         frame;
 			SRef<Keyframe>										keyframe;
 			// Get current image
 			if (camera->getNextImage(view) != FrameworkReturnCode::_SUCCESS)
-				break;
-            // Image pre-filtering
-            if (imageFilter)
-                imageFilter->filter(view, filteredView);
-            else
-            {
-                filteredView = view;
-            }
+				break;            
+			// convert to grey image
+			SRef<Image> greyImage;
+			if (view->getImageLayout() != Image::ImageLayout::LAYOUT_GREY)
+				imageConvertor->convert(view, greyImage, datastructure::Image::ImageLayout::LAYOUT_GREY);
+			else
+				greyImage = view;
+			// Image pre-filtering
+			SRef<Image> filteredImage;
+			if (USING_FILTER)
+				imageFilter->filter(greyImage, filteredImage);
+			else
+				filteredImage = greyImage;
             // feature extraction (use directly a descriptor extractor from image if defined in the configuration file
-            if (descriptorExtractorFromImage)
-                descriptorExtractorFromImage->extract(filteredView, keypoints, descriptors);
-            else
-            {
-                keypointsDetector->detect(filteredView, keypoints);
-                if (keypoints.size() == 0)
-                    continue;
-                descriptorExtractor->extract(filteredView, keypoints, descriptors);
+            if (USING_POPSIFT && descriptorExtractorFromImage)
+                descriptorExtractorFromImage->extract(filteredImage, keypoints, descriptors);
+            else {
+				keypointsDetector->detect(filteredImage, keypoints);
+				if (keypoints.size() == 0)
+					continue;
+				descriptorExtractor->extract(filteredImage, keypoints, descriptors);
             }
 			// undistort keypoints
 			undistortKeypoints->undistort(keypoints, undistortedKeypoints);
-            frame = xpcf::utils::make_shared<Frame>(keypoints, undistortedKeypoints, descriptors, filteredView);
-			// tracking
-			if (tracking->process(frame, displayImage) == FrameworkReturnCode::_SUCCESS) {
-				// used for display
-				framePoses.push_back(frame->getPose());
-				// draw cube
-                overlay3D->draw(frame->getPose(), displayImage);
-				// mapping
-				if (mapping->process(frame, keyframe) == FrameworkReturnCode::_SUCCESS) {
-					LOG_DEBUG("New keyframe id: {}", keyframe->getId());
-					// Local bundle adjustment
-					std::vector<uint32_t> bestIdx, bestIdxToOptimize;
-					covisibilityGraphManager->getNeighbors(keyframe->getId(), minWeightNeighbor, bestIdx);
-					if (bestIdx.size() < NB_LOCALKEYFRAMES)
-						bestIdxToOptimize = bestIdx;
-					else
-						bestIdxToOptimize.insert(bestIdxToOptimize.begin(), bestIdx.begin(), bestIdx.begin() + NB_LOCALKEYFRAMES);
-					bestIdxToOptimize.push_back(keyframe->getId());
-					LOG_DEBUG("Nb keyframe to local bundle: {}", bestIdxToOptimize.size());
-					double bundleReprojError = bundler->bundleAdjustment(calibration, distortion, bestIdxToOptimize);
-					// local map pruning
-					std::vector<SRef<CloudPoint>> localPointCloud;
-					mapManager->getLocalPointCloud(keyframe, 1.0, localPointCloud);
-					int nbRemovedCP = mapManager->pointCloudPruning(localPointCloud);
-					std::vector<SRef<Keyframe>> localKeyframes;
-					keyframesManager->getKeyframes(bestIdx, localKeyframes);
-					int nbRemovedKf = mapManager->keyframePruning(localKeyframes);
-					LOG_DEBUG("Nb of pruning cloud points / keyframes: {} / {}", nbRemovedCP, nbRemovedKf);
-					// loop closure
-					countNewKeyframes++;
-					if (countNewKeyframes >= NB_NEWKEYFRAMES_LOOP) {
-						SRef<Keyframe> detectedLoopKeyframe;
-						Transform3Df sim3Transform;
-						std::vector<std::pair<uint32_t, uint32_t>> duplicatedPointsIndices;
-						if (loopDetector->detect(keyframe, detectedLoopKeyframe, sim3Transform, duplicatedPointsIndices) == FrameworkReturnCode::_SUCCESS) {
-							// detected loop keyframe
-							LOG_INFO("Detected loop keyframe id: {}", detectedLoopKeyframe->getId());
-							LOG_INFO("Nb of duplicatedPointsIndices: {}", duplicatedPointsIndices.size());
-							LOG_INFO("sim3Transform: \n{}", sim3Transform.matrix());
-							// performs loop correction 
-							countNewKeyframes = 0;
-							loopCorrector->correct(keyframe, detectedLoopKeyframe, sim3Transform, duplicatedPointsIndices);
-							// Loop optimisation
-							bundler->bundleAdjustment(calibration, distortion);
-							// map pruning
-							mapManager->pointCloudPruning();
-							mapManager->keyframePruning();
-						}
-					}					
-				}
-				// update reference keyframe
-				if (keyframe) {
-					tracking->updateReferenceKeyframe(keyframe);
+            frame = xpcf::utils::make_shared<Frame>(keypoints, undistortedKeypoints, descriptors, view);
+			// check bootstrap
+			if (!bootstrapOk) {
+				Transform3Df pose;
+				if (hasPose && (fiducialMarkerPoseEstimator->estimate(greyImage, pose) == FrameworkReturnCode::_SUCCESS))
+					frame->setPose(pose);
+				if (bootstrapper->process(frame, displayImage) == FrameworkReturnCode::_SUCCESS) {
+					keyframesManager->getKeyframe(1, keyframe2);
+					tracking->updateReferenceKeyframe(keyframe2);
+					framePoses.push_back(keyframe2->getPose());
+					bootstrapOk = true;
+					LOG_INFO("Number of initial point cloud: {}", pointCloudManager->getNbPoints());
+					LOG_INFO("Number of initial keyframes: {}", keyframesManager->getNbKeyframes());
 				}
 			}
-			count++;
-
+			else {
+				// tracking
+				if (tracking->process(frame, displayImage) == FrameworkReturnCode::_SUCCESS) {
+					// used for display
+					framePoses.push_back(frame->getPose());					
+					// mapping
+					if (mapping->process(frame, keyframe) == FrameworkReturnCode::_SUCCESS) {
+						LOG_DEBUG("New keyframe id: {}", keyframe->getId());
+						// Local bundle adjustment
+						std::vector<uint32_t> bestIdx, bestIdxToOptimize;
+						covisibilityGraphManager->getNeighbors(keyframe->getId(), minWeightNeighbor, bestIdx);
+						if (bestIdx.size() < NB_LOCALKEYFRAMES)
+							bestIdxToOptimize = bestIdx;
+						else
+							bestIdxToOptimize.insert(bestIdxToOptimize.begin(), bestIdx.begin(), bestIdx.begin() + NB_LOCALKEYFRAMES);
+						bestIdxToOptimize.push_back(keyframe->getId());
+						LOG_DEBUG("Nb keyframe to local bundle: {}", bestIdxToOptimize.size());
+						double bundleReprojError = bundler->bundleAdjustment(calibration, distortion, bestIdxToOptimize);
+						// local map pruning
+						std::vector<SRef<CloudPoint>> localPointCloud;
+						mapManager->getLocalPointCloud(keyframe, 1.0, localPointCloud);
+						int nbRemovedCP = mapManager->pointCloudPruning(localPointCloud);
+						std::vector<SRef<Keyframe>> localKeyframes;
+						keyframesManager->getKeyframes(bestIdx, localKeyframes);
+						int nbRemovedKf = mapManager->keyframePruning(localKeyframes);
+						LOG_DEBUG("Nb of pruning cloud points / keyframes: {} / {}", nbRemovedCP, nbRemovedKf);
+						// loop closure
+						countNewKeyframes++;
+						if (countNewKeyframes >= NB_NEWKEYFRAMES_LOOP) {
+							SRef<Keyframe> detectedLoopKeyframe;
+							Transform3Df sim3Transform;
+							std::vector<std::pair<uint32_t, uint32_t>> duplicatedPointsIndices;
+							if (loopDetector->detect(keyframe, detectedLoopKeyframe, sim3Transform, duplicatedPointsIndices) == FrameworkReturnCode::_SUCCESS) {
+								// detected loop keyframe
+								LOG_INFO("Detected loop keyframe id: {}", detectedLoopKeyframe->getId());
+								LOG_INFO("Nb of duplicatedPointsIndices: {}", duplicatedPointsIndices.size());
+								LOG_INFO("sim3Transform: \n{}", sim3Transform.matrix());
+								// performs loop correction 
+								countNewKeyframes = 0;
+								loopCorrector->correct(keyframe, detectedLoopKeyframe, sim3Transform, duplicatedPointsIndices);
+								// Loop optimisation
+								bundler->bundleAdjustment(calibration, distortion);
+								// map pruning
+								mapManager->pointCloudPruning();
+								mapManager->keyframePruning();
+							}
+						}
+					}
+					// update reference keyframe
+					if (keyframe) {
+						tracking->updateReferenceKeyframe(keyframe);
+					}
+				}
+				// display point cloud
+				if (!fnDisplay(framePoses))
+					break;
+			}
+			// draw cube
+			if (!frame->getPose().isApprox(Transform3Df::Identity()))
+				overlay3D->draw(frame->getPose(), displayImage);
 			// display matches and a cube on the origin of coordinate system
-            if (imageViewer->display(displayImage) == FrameworkReturnCode::_STOP)
-				break;
-
-			// display point cloud
-			if (!fnDisplay(framePoses))
-				break;
+			if (imageViewer->display(displayImage) == FrameworkReturnCode::_STOP)
+				break;			
+			count++;
 		}
 
 		// display stats on frame rate
