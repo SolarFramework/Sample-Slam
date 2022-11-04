34cb3a553c0b79e66ea074770750adf9ab178e41
Jean-Marie HENAFF <Jean-Marie.HENAFF@b-com.com>
2022-11-04 17:26:05 +0100

    fix: create destination hierarchy when downloading popsift_uint8.fbow

--- installData.sh
+++ installData.sh
@@ -3,7 +3,7 @@ curl https://github.com/SolarFramework/SolARModuleFBOW/releases/download/fbowVoc
 unzip -o fbow_voc.zip -d ./data
 rm fbow_voc.zip
 
-curl https://artifact.b-com.com/solar-generic-local/FbowVoc/popsift_uint8.fbow -L -o ./data/fbow_voc/popsift_uint8.fbow
+curl https://artifact.b-com.com/solar-generic-local/FbowVoc/popsift_uint8.fbow -L -o ./data/fbow_voc/popsift_uint8.fbow --create-dirs
 
 # Download  captures
 curl https://vision.in.tum.de/rgbd/dataset/freiburg3/rgbd_dataset_freiburg3_long_office_household_validation-rgb.avi -L -o ./data/rgbd_dataset_freiburg3_long_office_household_validation-rgb.avi
